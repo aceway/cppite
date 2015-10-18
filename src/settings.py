bc7d74cdbf00101622f28d1ea9ae36d8fdc75cee
aceway <aceway@qq.com>
2015-10-18 22:41:02 +0800

    move file,dir

--- src/settings.py
+++ src/settings.py
@@ -1,25 +1,6 @@
-#!/usr/bin/env python
-# -*- encoding:utf-8 -*-
-class color:
-    FG_BLAcK    = "\033[30m"    # 将字符的显示颜色改为黑色
-    FG_RED      = "\033[31m"    # 将字符的显示颜色改为红色
-    FG_GREEN    = "\033[32m"    # 将字符的显示颜色改为绿色
-    FG_YELLOW   = "\033[33m"    # 将字符的显示颜色改为黄色
-    FG_BLUE     = "\033[34m"    # 将字符的显示颜色改为蓝色
-    FG_PINK     = "\033[35m"    # 将字符的显示颜色改为紫色
-    FG_WHITE    = "\033[37m"    # 将字符的显示颜色改为灰色
-    FG_LIGHT_BLUE= "\033[36m"   # 将字符的显示颜色改为淡蓝色
-    
-    BG_BLACK    = "\033[40m"    # 将背景色设置为黑色
-    BG_RED      = "\033[41m"    # 将背景色设置为红色
-    BG_GREEN    = "\033[42m"    # 将背景色设置为绿色
-    BG_YELLOW   = "\033[43m"    # 将背景色设置为黄色
-    BG_BLUE     = "\033[44m"    # 将背景色设置为蓝色
-    BG_PINK     = "\033[45m"    # 将背景色设置为紫色
-    BG_WHITE    = "\033[47m"    # 将背景色设置为灰色 
-    BG_LIGHT_BLUE= "\033[46m"  # 将背景色设置为淡蓝色=    
-    END         ="\033[0;00m" 
+#!/usr/bin/evn python
 
-
-
-root_tip="{cs}CPPITE> {ce}".format( cs=color.FG_GREEN, ce=color.END )
+bin="./"
+CXX="gcc"
+includes=[]
+libs=[]
