26c781807937038ec2c4fbfd4413ae2c60decd1b
aceway <aceway@qq.com>
2015-10-31 10:18:24 +0800

    add stdint.h for c++ default header include.

--- src/py/cpp_fragment_tmpl.py
+++ src/py/cpp_fragment_tmpl.py
@@ -9,6 +9,9 @@ hpp_tmpl="""#ifndef __FRAGMENT_HPP__
 #include <map>
 #include <list>
 
+// linux int type define; should be remore/add by system dependent in the future version.
+#include <stdint.h>  
+
 {includes}
 
 void fragment_container();
